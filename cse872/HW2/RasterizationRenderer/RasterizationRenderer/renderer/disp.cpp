ce7d7eb6b9ef640fdafdfb99ed6084f410fae046
Brett Lesnau <lesnaubr@gmail.com>
2013-11-12 01:02:17 -0500

    Just making modifications pointed out in HW description

--- cse872/HW2/RasterizationRenderer/RasterizationRenderer/renderer/disp.cpp
+++ cse872/HW2/RasterizationRenderer/RasterizationRenderer/renderer/disp.cpp
@@ -4,113 +4,122 @@
 
 GzDisplay::GzDisplay(GzDisplayClass disp_class,int xRes,int yRes)
 {
-/* create a display:
-  -- allocate memory for indicated class and resolution
-  -- pass back pointer to GzDisplay object in display
-*/
-	this->xres = xRes;
-	this->yres = yRes;
-	this->disp_class = disp_class;
-	this->open = false;
-	this->fbuf = new GzPixel[xRes*yRes];
-	this->image = new BYTE[xRes*yRes*3];
+   /* create a display:
+   -- allocate memory for indicated class and resolution
+   -- pass back pointer to GzDisplay object in display
+   */
+   this->xres = xRes;
+   this->yres = yRes;
+   this->disp_class = disp_class;
+   this->open = false;
+   this->fbuf = new GzPixel[xRes*yRes];
+   this->image = new BYTE[xRes*yRes*3];
 }
 
 
 GzDisplay::~GzDisplay()
 {
-/* clean up, free memory */
-	delete[] fbuf;
-	delete[] image;
+   /* clean up, free memory */
+   delete[] fbuf;
+   delete[] image;
 }
 
 
 int GzDisplay::GetParams(int	*xRes, int	*yRes, GzDisplayClass	*disp_class)
 {
-/* pass back values for an open display */
-	*xRes = this->xres;
-	*yRes = this->yres;
-	*disp_class = this->disp_class;
+   /* pass back values for an open display */
+   *xRes = this->xres;
+   *yRes = this->yres;
+   *disp_class = this->disp_class;
 
-	return GZ_SUCCESS;
+   return GZ_SUCCESS;
 }
 
 
 int GzDisplay::Init()
 {
-/* set everything to some default values - start a new frame */
-	int size = xres * yres;
-	for (int i = 0; i < size; i++) {
-		fbuf[i].alpha = (1<<12)-1;
-		fbuf[i].red = 0x60<<4;
-		fbuf[i].green = 0x70<<4;
-		fbuf[i].blue = 0x60<<4;
-		fbuf[i].z = INT_MAX;
-	}
+   /* set everything to some default values - start a new frame */
+   int size = xres * yres;
+   for (int i = 0; i < size; i++) {
+      fbuf[i].alpha = (1<<12)-1;
+      fbuf[i].red = 0x60<<4;
+      fbuf[i].green = 0x70<<4;
+      fbuf[i].blue = 0x60<<4;
+      fbuf[i].z = INT_MAX;
+   }
 
-	open = true;
-	return GZ_SUCCESS;
+   open = true;
+   return GZ_SUCCESS;
 }
 
 inline int index(const int&i, const int &j, const int &res)
 {
-	return res*j+i;
+   return res*j+i;
 }
 
-int GzDisplay::Put(int	i, int	j, GzIntensity	r, GzIntensity	g, GzIntensity	b, GzIntensity	a, GzDepth		z)
+int GzDisplay::Put(int i, int	j, GzIntensity	r, GzIntensity	g, GzIntensity	b, GzIntensity	a, GzDepth z)
 {
-/* write pixel values into the display */
-	if (i<0 || i >=xres || j < 0 || j >= yres)
-		return GZ_FAILURE;
+   /* write pixel values into the display */
+   if( i<0 || i >=xres || j < 0 || j >= yres )
+   {
+      return GZ_FAILURE;
+   }
 
-	GzPixel & pixel = fbuf[index(i,j,xres)];
+   GzPixel & pixel = fbuf[index(i,j,xres)];
 
-	//Todo: replace Put() by the proper code
+   if( pixel.z >= z )
+   {
+      pixel.alpha = a;
+      pixel.red = r;
+      pixel.green = g;
+      pixel.blue = b;
+      pixel.z = r;
+   }
 
-	return GZ_SUCCESS;
+   return GZ_SUCCESS;
 }
 
 
 int GzDisplay::Get(int	i,int	j,GzIntensity	*r,GzIntensity	*g,GzIntensity	*b,GzIntensity	*a,GzDepth		*z)
 {
-/* pass back pixel value in the display */
-/* check display class to see what vars are valid */
-	if (i<0 || i >=xres || j < 0 || j >= yres)
-		return GZ_FAILURE;
+   /* pass back pixel value in the display */
+   /* check display class to see what vars are valid */
+   if (i<0 || i >=xres || j < 0 || j >= yres)
+      return GZ_FAILURE;
 
-	GzPixel & pixel = fbuf[index(i,j,yres)];
-	*a = pixel.alpha;
-	*r = pixel.red;
-	*g = pixel.green;
-	*b = pixel.blue;
-	*z = pixel.z;
+   GzPixel & pixel = fbuf[index(i,j,yres)];
+   *a = pixel.alpha;
+   *r = pixel.red;
+   *g = pixel.green;
+   *b = pixel.blue;
+   *z = pixel.z;
 
-	return GZ_SUCCESS;
+   return GZ_SUCCESS;
 }
 
 
 /* Convert an intensity value to a 8-bit intensity value */
 static char ByteIntensity(GzIntensity intensity)
 {
-	intensity >>= 4;
-	return (char)((intensity > 255) ? 255 : intensity);
+   intensity >>= 4;
+   return (char)((intensity > 255) ? 255 : intensity);
 }
 
 int GzDisplay::Flush()
 {
-	if (!open)
-		return GZ_FAILURE;
+   if (!open)
+      return GZ_FAILURE;
 
-	/* write pixels */
+   /* write pixels */
 
-	int size = xres * yres;
-	for (int i = 0; i < size; i++) {
-		GzPixel & pixel = fbuf[i];
-		image[3*i+2] = ByteIntensity(pixel.red);
-		image[3*i+1] = ByteIntensity(pixel.green);
-		image[3*i+0] = ByteIntensity(pixel.blue);
-	}
-	return GZ_SUCCESS;
+   int size = xres * yres;
+   for (int i = 0; i < size; i++) {
+      GzPixel & pixel = fbuf[i];
+      image[3*i+2] = ByteIntensity(pixel.red);
+      image[3*i+1] = ByteIntensity(pixel.green);
+      image[3*i+0] = ByteIntensity(pixel.blue);
+   }
+   return GZ_SUCCESS;
 }
 
 
